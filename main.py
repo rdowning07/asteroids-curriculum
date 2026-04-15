5110f17de3143c8040af6a0c91331640b46d10b1
Rob Downing <redowning@gmail.com>
2026-04-14 22:38:26 -0700

    feat: game loop with delta time

---
+++ main.py
@@ -0,0 +1,32 @@
+import pygame
+import math
+
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Asteroids")
+
+#set up the clock for a decent framerate
+clock = pygame.time.Clock()
+
+#game loop
+running = True
+while running:
+    #check for events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+
+    #fill the screen with black
+    screen.fill((0, 0, 0))
+
+    #update the display
+    pygame.display.flip()
+    #cap the framerate at 60fps
+    dt = clock.tick(60) / 1000.0 # Delta time in seconds
+
+pygame.quit()
